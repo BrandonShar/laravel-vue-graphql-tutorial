3f5b4b733183dab07e57c1f1823f495cd6afd245
Brandon Shar <brandon.shar@icloud.com>
2018-11-18 18:21:02 -0500

    Set up shopping cart page just to dump some data

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,7 +17,7 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('shopping-cart-page', require('./components/ShoppingCartPage.vue'));
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
---
+++ resources/js/components/ShoppingCartPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <ApolloQuery :query="$options.query">
+        <template slot-scope="{result: {loading, data, error }}">
+            <div v-if="loading">Loading...</div>
+            <div v-else-if="data">{{ data }}</div>
+        </template>
+    </ApolloQuery>
+</template>
+
+<script>
+    import gql from 'graphql-tag';
+
+    export default {
+        query: gql`
+            query ShoppingCartPage {
+                me {
+                    id
+                    shoppingCart {
+                        id
+                        total
+                        totalBeforeSavings
+                        savings
+                        products {
+                            id
+                            name
+                            price
+                            savings
+                            quantity
+                        }
+                    }
+                }
+                products {
+                    id
+                    name
+                    price
+                    savings
+                }
+            }
+        `
+    }
+</script>
